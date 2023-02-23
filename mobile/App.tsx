fca261d223252c35fd40aec4d97f68dc1eb6caff
Felipe-Gs <felipegs2905@gmail.com>
2023-02-23 12:39:27 -0300

    refactory

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Routes } from "./Routes";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthContextProvider>
+        <PaperProvider>
+          <Routes />
+        </PaperProvider>
+      </AuthContextProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
