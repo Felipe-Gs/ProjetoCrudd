9b1923ade8e2bd11867b576c2a71bc5d0d4f0840
Felipe-Gs <felipegs2905@gmail.com>
2023-02-28 13:17:51 -0300

    feat: rotas de atualizar dados e buscar dados especificos criadas

--- server/src/routes.js
+++ server/src/routes.js
@@ -119,6 +119,86 @@ router.delete("/delete/:id", (req, res) => {
   }
 });
 
+router.patch("/atualizarDados/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const atualizarBody = Z.object({
+      // nome, sobrenome, email, senha, profissao, idade
+      nome: Z.string(),
+      sobrenome: Z.string(),
+      email: Z.string(),
+      senha: Z.string().min(3),
+      profissao: Z.string(),
+      idade: Z.number(),
+    }).required();
+
+    const validData = atualizarBody.parse(req.body);
+    const { nome, sobrenome, email, senha, profissao, idade } = validData;
+
+    const campos = [];
+    if (nome) campos.push(`nome = '${nome}'`);
+    if (sobrenome) campos.push(`sobrenome = '${sobrenome}'`);
+    if (email) campos.push(`email = '${email}'`);
+    if (senha) campos.push(`senha = '${senha}'`);
+    if (profissao) campos.push(`profissao = '${profissao}'`);
+    if (idade) campos.push(`idade = ${idade}`);
+
+    const query = `UPDATE usuario SET ${campos.join(", ")} WHERE id = ${id}`;
+    client.query(query, (err, result) => {
+      if (err) {
+        return res.status(404).send({
+          message: "erro inesperado",
+        });
+      } else {
+        const realizarBuscar = `SELECT * FROM usuario WHERE id = ${id}`;
+        client.query(realizarBuscar, (err, result) => {
+          if (err) {
+            return res.status(404).send({
+              message: "erro inesperado",
+            });
+          } else if (result.rows.length === 0) {
+            return res.status(202).send({
+              message: "Nao existe usuario cadastrado com esse id",
+            });
+          } else {
+            return res.status(200).send({
+              usuario: result.rows[0],
+            });
+          }
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Nao foi possivel atualizar os dados!",
+    });
+  }
+});
+
+router.get("/buscarDadosEspecificos/:numero", (req, res) => {
+  const numero = req.params.numero;
+  try {
+    const query = `SELECT * FROM usuario WHERE id > ${numero}`;
+    client.query(query, (err, result) => {
+      if (err) {
+        return res.status(404).send({
+          message: "erro inesperado",
+        });
+      } else if (result.rows.length === 0) {
+        return res.status(202).send({
+          message: "nao ha dados maiores que o valor informado!",
+        });
+      } else {
+        return res.status(200).send({
+          usuario: result.rows,
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // router.post("/usuario", async (req, res) => {
 //   try {
 //     const { nome, sobrenome, email, senha, profissao, idade } = req.body;
